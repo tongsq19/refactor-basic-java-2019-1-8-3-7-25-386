fc7c9e51c73e65ca2c0cc78d5cd62e2b72fd27b5
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-12 01:08:59 +0800

    Better Refractor scheme for Practice1

--- src/main/java/practice1/Security.java
+++ src/main/java/practice1/Security.java
@@ -12,16 +12,15 @@ public class Security {
 
     public boolean hasAccess(User user, Permission permission, ImmutableList<Permission> permissions) {
 
-
         if (user == null) return false;
-
         if (permission == null) return false;
-
         if (permissions.size() == 0) return false;
 
         if (securityChecker.isAdmin()) return true;
+        if (securityChecker.checkPermission(user, permission)) return true;
+        if (permissions.contains(permission)) return true;
 
-        return this.securityChecker.checkPermission(user, permission) || permissions.contains(permission);
+        return false;
 
     }
 }
